658f66a6c6f22235f6faf36cc0def49679f271ad
Alex Ramos <oemaildoalexr@gmail.com>
2021-12-18 15:10:55 -0300

    Adiciona um modal ao botão de acionar serviço

---
+++ script.js
@@ -0,0 +1,14 @@
+const tasks = [
+  {
+    "id": "1",
+    "title": "my title",
+    "description": "my body",
+    "imgURL": "https://f.i.uol.com.br/fotografia/2021/12/18/163984743961be160f1cbd8_1639847439_3x2_lg.jpg"
+  },
+  {
+    "id": "2",
+    "title": "my title 2",
+    "description": "my body 2",
+    "imgURL": "https://s2.glbimg.com/D--AuJs5HWmBZYugeonPQRGLkAk=/0x0:1920x1080/540x304/smart/filters:max_age(3600)/https://i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2021/O/S/JzAXIwTBGU6gZZRoCgmQ/denuncia-preconceito-0001s03-frame-9872.jpeg"
+  },
+]
