a66e5aec414d1d6827da0a55711cc4a169d28392
Alex Ramos <oemaildoalexr@gmail.com>
2021-12-28 19:16:05 -0300

    Altera a nomenclatura "curso" por "servico"

--- public/js/script.js
+++ public/js/script.js
@@ -1,10 +1,17 @@
 window.onload = function () {
-  //Lista de Cursos
-  let listaCursos = [];
+  const listaServicosRenderizada = document.querySelector("#tabelaServicos tbody")
+  
+  //Lista de Servicos
+  let listaServicos = [
+    {nome: 'Desenvolvimento Web', imagem: 'imagens/ilustra-marketing.png', descricao: 'Consequatur debitis ipsa numquam illum placeat quod deleniti.'},
+    {nome: 'Marketing Digital', imagem: 'imagens/ilustra-marketing.png', descricao: 'Consequatur debitis ipsa numquam illum placeat quod deleniti.'},
+    {nome: 'Consultoria UX', imagem: 'imagens/ilustra-ux.png', descricao: 'Consequatur debitis ipsa numquam illum placeat quod deleniti.'},
+  ];
+
 
   //Acessando botões
-  let btnSalvarCurso = document.querySelector("#btnSalvar");
-  let btnEditarCurso = document.querySelector("#btnEditarCurso");
+  let btnSalvarServico = document.querySelector("#btnSalvar");
+  let btnEditarServico = document.querySelector("#btnEditarServico");
 
   //Criar Alerta
   function criarAlerta(idAlerta, texto) {
@@ -18,97 +25,99 @@ window.onload = function () {
     alerta.appendChild(textoAlerta);
   }
 
-  //Função Criar Novo Curso
-  const cadastrarCurso = () => {
-    let nomeCurso = document.querySelector("#nomeCurso").value;
-    let imgCurso = document.querySelector("#imgCurso").value;
-    let descricaoCurso = document.querySelector("#descricaoCurso").value;
+  //Função Criar Novo Servico
+  const cadastrarServico = () => {
+    let nomeServico = document.querySelector("#nomeServico").value;
+    let imgServico = document.querySelector("#imgServico").value;
+    let descricaoServico = document.querySelector("#descricaoServico").value;
 
-    if (nomeCurso == "" || imgCurso == "" || descricaoCurso == "") {
+    if (nomeServico == "" || imgServico == "" || descricaoServico == "") {
       criarAlerta("#criarAlerta", "Ops! Preencha todos os campos!");
     } else {
-      let curso = {
-        nome: nomeCurso,
-        imagem: imgCurso,
-        descricao: descricaoCurso,
+      let servico = {
+        nome: nomeServico,
+        imagem: imgServico,
+        descricao: descricaoServico,
       };
 
-      const cursoindex = listaCursos.push(curso) - 1;
+      const servicoindex = listaServicos.push(servico) - 1;
 
-      const novoCurso = document.createElement("tr");
-      novoCurso.id = "curso-index" + cursoindex;
-      novoCurso.innerHTML = `
-      <td class="cursoNome">${nomeCurso}</td>
+      const novoServico = document.createElement("tr");
+      novoServico.id = "servico-index" + servicoindex;
+      novoServico.innerHTML = `
+      <td class="servicoNome">${nomeServico}</td>
       <td>
-        <img src="${imgCurso}" class="img-fluid cursoImg" />
+        <img src="${imgServico}" class="img-fluid servicoImg" />
       </td>
-      <td class="cursoDescricao">${descricaoCurso}</td>
+      <td class="servicoDescricao">${descricaoServico}</td>
       <td>
-        <button class="btn btn-secondary m-1" onclick="editarCurso(${cursoindex})">editar</button>
+        <button class="btn btn-secondary m-1" onclick="editarServico(${servicoindex})">editar</button>
         <button class="btn btn-danger m-1" >excluir</button>
       </td>
     `;
+
+
       $("#modalCadastro").modal("hide");
       document.getElementById("cadastradoComSucesso").innerHTML = `
       <div class="alert alert-success alert-dismissible fade show" role="alert">
-        Curso - <strong>${nomeCurso}</strong> - Cadastrado com Sucesso!
+        Servico - <strong>${nomeServico}</strong> - Cadastrado com Sucesso!
         <button type="button" class="close" data-dismiss="alert" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
       `;
       document.getElementById("formulario").reset();
-      document.querySelector("#tabelaCursos tbody").appendChild(novoCurso);
-      console.log(listaCursos);
+      document.querySelector("#tabelaServicos tbody").appendChild(novoServico);
+      console.log(listaServicos);
     }
   };
 
   //Eventos
-  btnSalvarCurso.addEventListener("click", cadastrarCurso);
+  btnSalvarServico.addEventListener("click", cadastrarServico);
   
 
-  // Função editar curso
-  window.editarCurso = function editarCurso(cursoindex) {
+  // Função editar servico
+  window.editarServico = function editarServico(servicoindex) {
     $("#modalEditar").modal("show");
 
-    let novoNomeCurso = document.querySelector("#nomeCursoEditar");
-    let novoImgCurso = document.querySelector("#imgCursoEditar");
-    let novoDescricaoCurso = document.querySelector("#descricaoCursoEditar");
+    let novoNomeServico = document.querySelector("#nomeServicoEditar");
+    let novoImgServico = document.querySelector("#imgServicoEditar");
+    let novoDescricaoServico = document.querySelector("#descricaoServicoEditar");
 
-    // Pegando o valor atual do curso para mostrar no form do modal
-    novoNomeCurso.value = listaCursos[cursoindex].nome;
-    novoImgCurso.value = listaCursos[cursoindex].imagem;
-    novoDescricaoCurso.value = listaCursos[cursoindex].descricao;
+    // Pegando o valor atual do servico para mostrar no form do modal
+    novoNomeServico.value = listaServicos[servicoindex].nome;
+    novoImgServico.value = listaServicos[servicoindex].imagem;
+    novoDescricaoServico.value = listaServicos[servicoindex].descricao;
 
     //evento
-    btnEditarCurso.addEventListener("click", editarCurso);
+    btnEditarServico.addEventListener("click", editarServico);
     $("#modalEditar").on("hide.bs.modal", function () {
-      btnEditarCurso.removeEventListener("click", editarCurso);
+      btnEditarServico.removeEventListener("click", editarServico);
     });
 
-    function editarCurso() {
-      const curso = listaCursos[cursoindex];
+    function editarServico() {
+      const servico = listaServicos[servicoindex];
 
-      curso.nome = novoNomeCurso.value;
-      curso.imagem = novoImgCurso.value;
-      curso.descricao = novoDescricaoCurso.value;
+      servico.nome = novoNomeServico.value;
+      servico.imagem = novoImgServico.value;
+      servico.descricao = novoDescricaoServico.value;
 
-      if (curso.nome == "" || curso.imagem == "" || curso.descricao == "") {
+      if (servico.nome == "" || servico.imagem == "" || servico.descricao == "") {
 
         criarAlerta("#criarAlertaEditar", "Ops! Preencha todos os campos!");
       } else {
-        let cursoEditado = document.querySelector(`#curso-index${cursoindex}`);
-        // Atualizando o nome do curso
-        const cursoNome = cursoEditado.querySelector(".cursoNome");
-        cursoNome.textContent = curso.nome;
+        let servicoEditado = document.querySelector(`#servico-index${servicoindex}`);
+        // Atualizando o nome do servico
+        const servicoNome = servicoEditado.querySelector(".servicoNome");
+        servicoNome.textContent = servico.nome;
 
-        // Atualizando a imagem do curso
-        const cursoImg = cursoEditado.querySelector(".cursoImg");
-        cursoImg.src = curso.imagem;
+        // Atualizando a imagem do servico
+        const servicoImg = servicoEditado.querySelector(".servicoImg");
+        servicoImg.src = servico.imagem;
 
-        // Atualizando a descrição do curso
-        const cursoDescricao = cursoEditado.querySelector(".cursoDescricao");
-        cursoDescricao.textContent = curso.descricao;
+        // Atualizando a descrição do servico
+        const servicoDescricao = servicoEditado.querySelector(".servicoDescricao");
+        servicoDescricao.textContent = servico.descricao;
 
         $("#modalEditar").modal("hide");
       }
